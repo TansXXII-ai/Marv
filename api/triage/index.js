f760e900e1e701f0573ce6ea33fa6b14fd54f646
TansXXII-ai <xxiiuk@gmail.com>
2025-10-17 12:04:33 +0100

    Add CORS support and update response message

--- api/triage/index.js
+++ api/triage/index.js
@@ -1,6 +1,22 @@
+const Busboy = require("busboy");
+
 module.exports = async function (context, req) {
   context.log('Triage function called');
   
+  // CORS preflight
+  if (req.method === "OPTIONS") {
+    context.res = {
+      status: 204,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type"
+      }
+    };
+    return;
+  }
+
+  // Simple response for now
   context.res = {
     status: 200,
     headers: {
@@ -9,8 +25,8 @@ module.exports = async function (context, req) {
     },
     body: JSON.stringify({
       ok: true,
-      message: "Triage function is alive!",
-      method: req.method
+      message: "Triage function works!",
+      busboy_available: typeof Busboy !== 'undefined'
     })
   };
 };
